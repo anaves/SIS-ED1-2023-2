9fa799411b911a55add51c7bec20d7c970115f13
Alysson Naves <anaves@gmail.com>
2023-08-22 21:53:02 -0300

    aula 22ago23

--- 01-vetores/exercicios/exercicio05.py
+++ 01-vetores/exercicios/exercicio05.py
@@ -1,3 +1,13 @@
 """
-Escreva um programa que remova todos os elementos de uma lista que também estão presentes em outra lista.
+Escreva um programa que remova todos os elementos da primeira lista que também estão presentes na segunda lista.
+A = [4,5,2,3,1,2,5] = {1,2,3,4,5}
+B = [3,1,5,8,9]     = {1,3,5,8,9}
+Resultado = {2,4}
 """
+A = [4,5,2,3,1,2,5]
+B = [3,1,5,8,9]
+cA = set(A)
+cB = set(B)
+# trazer cA-cB
+cR = cA.difference(cB)
+print(cR)
